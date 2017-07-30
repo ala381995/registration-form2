f61f92e09dcd033a59c4289f9b558945c7a70800
ala381995 <30567003+ala381995@users.noreply.github.com>
2017-07-30 14:20:48 +0300

    Add files via upload

---
+++ regjs.js
@@ -0,0 +1,83 @@
+$(window).on('load', function () {
+ $(".error").hide(); 
+
+$(".btn").click(function(){
+	var fname=$("#fname");
+	var lname=$("#lname");
+	var email=$("#email");
+	var password=$("#pass");	
+	var cpass=$("#cpass");
+	var mGender=$("#mgndr");
+   var fGender=$("#fgndr");
+	var usrcity=$("#city");
+
+	validate(fname);
+	validate(lname);
+	validate(email);
+	validate(password);
+	validate(cpass);
+	validgender(mGender,fGender);
+	cityselect(usrcity);
+   //$(".error").show(); 
+
+});
+});
+
+
+function validate(id)
+{
+	var inputid=(id).val();
+	if(inputid==null||inputid=="")
+	{
+		//alert("innnn");
+		$(spanid).show(); 
+	}else
+	{
+				$(spanid).hide(); 
+
+	}
+}
+////////
+function validgender(mGender,fGender)  
+{ 
+ 
+x=0;  
+  
+if(mGender.checked)   
+{  
+x++;  
+} if(fGender.checked)  
+{  
+x++;   
+}  
+if(x==0)  
+{ 
+ $("#GenderError").show();
+
+   
+return false;  
+} 
+$("#GenderError").hide();
+return true; 
+}
+/////////////////
+function cityselect(usrcity)  
+{  var flag=false;
+if(flag==false)
+{
+	$("#cityerror").show();
+
+var op = document.getElementById("city").value;
+if (op=="option")
+ {
+flag=true;
+}
+else
+{
+
+	$("#cityerror").hide();
+}
+
+}
+} 
+///////////////////////
